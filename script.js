02e43c9532d2799fa3a4c95adfedda90425fe2b6
rani-motru <rani.motru@icloud.com>
2023-11-02 02:52:03 -0400

    added remove olaf parts and fixed issues

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-//list of  words for the game.
+//list of  hideWord for the game.
 
-let hideWord = ["CAT","DOG", "APPLE" ,"CHOCOLATE","BIRTHDAY","CAKE","PARIS","AMAZON","GOOGLE","FROZEN","DISNEY",
+var hideWord = ["CAT","DOG", "APPLE" ,"CHOCOLATE","BIRTHDAY","CAKE","PARIS","AMAZON","GOOGLE","FROZEN","DISNEY",
 "WORLD","HOME","JERRY","OHIO","DADDY","PERSIMMON","PIZZA","BENZ","WHITE","PURPLE","RED","IPHONE","COFFEE",
-"STARBUCKS","CONGRATS","MONKEY","JAVASCRIPT","ADMIRE","INSPIRE","HOLIDAY","CHRISTMAS","WONDER WOMAN","BATMAN","IRONMAN"];
+"STARBUCKS","CONGRATS","MONKEY","JAVASCRIPT","ADMIRE","INSPIRE","HOLIDAY","CHRISTMAS","WONDERWOMAN","BATMAN","IRONMAN"];
+var olafParts = [
+    ".head",
+    ".hands",
+    ".smallbody",
+    ".bigbody",
+    ".legs"
+];
 
+//To display the number of spcaes on the screen
+//Game loop here
+var letterbox = document.querySelector("#keyboard-container");
+// let emptyspaces =document.querySelector("#key-container");
+// let startBtn = document.querySelector("#startButton")
+// let reStartBtn = document.querySelector("#restartButton")
+// // let pressStart = false;
+// // let pressRetreat = false;
+// let lives = 5;
+// //  if(pressStart){
+var quesWord = genQuestion(hideWord);
+//  console.log(quesArray);
+letterbox.addEventListener("click",letterboxClick);
+populateBlank(quesWord);
+//  }
+
+// letterbox.addEventListener("click",letterboxClick);
 
 //FUNCTIONS:
 //1.TO GET THE RANDOM WORD.
-function genQues(word)
+function genQuestion(word)
 { return word[Math.floor(Math.random()*word.length)];
 }
 // let quesword= hideWord(randWord);
@@ -22,75 +46,79 @@ function genQues(word)
 // }
 //2.function to generate 
 //3.FUNCTION FOR CHECKING WINNER OR LOOSER.
- function winOrLoose(){
-  hideLetterElements = document.querySelectorAll(".hidden");
-       if(hideLetterElements.length === 0){
-        console.log('YAY!! YOu WIN!')
-      } else if(olafparts === 0){
-        document.querySelector("#olaf-container").innerHTMl ="<h2> YOU LOOSE!GAME OVER!</h2>";
-        letterbox.innerHTML = "<h2> The Word was ${quesWord}</h2>"
-        console.log("YOU LOOSE! ")
+function checkForLoser() {
+    if (olafParts.length === 0) {
+        document.querySelector("#olaf-container").innerHTML = "<h2> YOU LOOSE!GAME OVER!</h2>";
+        letterbox.innerHTML = `<h2> The Word was ${quesWord}</h2>`;
+    }
+}
+
+function checkForWinner() {
+    var hideLetterElements = document.querySelectorAll(".hidden");
+    if (hideLetterElements.length === 0) {
+        letterbox.innerHTML = "<h2>YAY!! YOu WIN!</h2>";
       }
  }
  
  //function to generate hidden word as blanks
   function populateBlank(blanks) {
-    let letters = blanks.split("");
-    let KeyContainer =document.querySelector("#Key-container");
-    let htmlLetters = "";
-    for(let letter of letters) {
-        let eachLetter = `<p class = "letter-space"><div class ="letter hidden">${letter}</div></p>`;
+    // var displayWord = "_ ".repeat(blanks.length);
+    // const newBlankDiv = document.createElement('div'); // creating a new div Element
+    // newBlankDiv.classList.add('blank');
+    //  newBlankDiv.textContent = displayWord;
+    //  let KeyContainer =document.getElementById("#empty-letter-container");
+    //  KeyContainer.appendChild(newBlankDiv);
+    var letters = blanks.split("");
+    var KeyContainer = document.querySelector("#empty-letter-container");
+    var htmlLetters = "";
+    for (var letter of letters) {
+        var eachLetter = `<p class="letter-container"><span class="letter hidden">${letter}</span></p>`; // do not change it to div, keep it span
         htmlLetters += eachLetter;
     }
-
     KeyContainer.innerHTML = htmlLetters;
   }
 
+     
+  // }
+
   //function to handle for letter clicks
-  function letterboxClick(event) {
-    let clickedElement = event.target;
-    if(!clickedElement.classList.contains("letter")||clickedElement.classList.contains("selected"))return;
+function letterboxClick(event) {
+    var clickedElement = event.target;
+    if (!clickedElement.classList.contains("letter") || clickedElement.classList.contains("selected")) return;
     clickedElement.classList.add("selected");
 
-  let clickedLetter = clickedElement.textContent;
-  let foundMatch = checkFoundMatch(clickedLetter);
-  //if the letter is found 
-  if(!foundMatch){
-    removeOlafPart();
-    winOrLoose();
-  } else {
-    winOrLoose();
-  }
-  }
-  // function to check for a match
-  function checkFoundMatch(clickedLetter){
-    let hideLetterElements = document.querySelectorAll(".hidden");
-     foundMatch = false;
-  for(let hideLetterElement of hideLetterElements){
-     let hiddenLetter = hideLetterElement.textContent;
-     if(hiddenLetter === clickedLetter){
-       hideLetterElement.classList.remove("hidden");
-       foundMatch = true;
-     }
-  } 
-  return foundMatch;
-  }
-
-//To display the number of spcaes on the screen
-//Game loop here
-let letterbox = document.querySelector("#button-container");
-let startBtn = document.querySelector("#startButton")
-let reStartBtn = document.querySelector("#restartButton")
-let pressStart = false;
-let pressRetreat = false;
-let lives = 5;
-
-if(pressStart){
-let quesWord= genQues(hideWord);
-//  console.log(quesArray);
-populateBlank(quesWord);
+    var clickedLetter = clickedElement.textContent;
+    var foundMatch = checkFoundMatch(clickedLetter);
+      //if the letter is found 
+    if (!foundMatch) {
+        removeOlafPart();
+        checkForLoser();
+    } else {
+        checkForWinner();
+    }
 }
 
+  // function to check for a match
+function checkFoundMatch(clickedLetter) {
+    var hideLetterElements = document.querySelectorAll(".hidden");
+    var foundMatch = false;
+    for (var hideLetterElement of hideLetterElements) {
+        var hiddenLetter = hideLetterElement.textContent;
+        if (hiddenLetter === clickedLetter) {
+            hideLetterElement.classList.remove("hidden");
+            foundMatch = true;
+        }
+      } 
+    return foundMatch;
+  }
 
 
-letterbox.addEventListener("click",letterboxClick);
+
+function removeOlafPart() {
+    var olafPart = olafParts.shift();
+    var partsToRemove = document.querySelectorAll(olafPart);
+
+    for (var partToRemove of partsToRemove) {
+        partToRemove.remove();
+    }
+}
